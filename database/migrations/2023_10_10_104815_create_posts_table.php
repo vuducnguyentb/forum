53e2437166bd58ddfb43431af5f3c90e0ee246db
vuducnguyentb <vuducnguyentb@gmail.com>
2023-10-10 18:03:13 +0700

    updat migration

---
+++ database/migrations/2023_10_10_104815_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->integer('forum_id');
+            $table->integer('user_id');
+            $table->integer('is_deleted');
+            $table->integer('is_approved');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
